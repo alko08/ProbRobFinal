2b0e5f54538696f149cbdfa466dfa0a4ca22f5fd
Alexander Koppel <32113635+alko08@users.noreply.github.com>
2024-12-10 17:53:58 -0500

    Upload of Package Files from Lab

--- scripts/slam_manager.py
+++ scripts/slam_manager.py
@@ -1,57 +1,79 @@
 #!/usr/bin/env python3
 
 import rospy
-import roslaunch
+import subprocess
 from std_msgs.msg import String
 
-class SlamManager:
-	def __init__(self):
-		rospy.init_node('slam_manager', anonymous=True)
-		self.slam_method = rospy.get_param('~default_slam', 'gmapping')
-		rospy.Subscriber('/slam_method', String, self.slam_method_callback)
-		
-		self.launch = roslaunch.scriptapi.ROSLaunch()
-		self.launch.start()
-		self.process = None
-		self.start_slam(self.slam_method)
+class SLAMManager:
+    def __init__(self):
+        rospy.init_node('slam_manager', anonymous=True)
 
-	def slam_method_callback(self,msg):
-		"""Callback to handle incoming SLAM method requests"""
-	
-		new_method = msg.data
-		if new_method != self.slam_method:
-			rospy.loginfo(f"Changing SLAM method from {self.slam_method} to {new_method}")
-			self.slam_method = new_method
-			self.start_slam(self.slam_method)
-		else:
-			rospy.loginfo(f"Received {new_method} but was already using it for SLAM.")
+        # Parameters
+        self.enable_teleop = rospy.get_param('~teleop', True)
+        self.enable_gazebo = rospy.get_param('~Gazebo', True)
+        self.enable_slam_toolbox = rospy.get_param('~slam_toolbox', True)
 
-	def start_slam(self, slam_method):
-		"""Start the SLAM method using ROS launch"""
-		if self.process: # used to stop any currently running launch file
-			rospy.loginfo(f"Stopping current SLAM method: {self.slam_method}")
-			self.process.stop()
-			self.process = None
-		package = "vanessa_alex"
-		executable = "slam.launch"
-		# Set the SLAM method argument
-		uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-		roslaunch.configure_logging(uuid)
-		launch_file = roslaunch.rlutil.resolve_launch_arguments([package, executable])[0]
-		args = {'slam_methods':slam_method}
-		rospy.loginfo(f"Starting SLAM method: {slam_method}")
-		self.process = self.launch.launch(roslaunch.Node(package, executable, output="screen", args=f"slam_methods:={slam_method}"))
-		
-	def shutdown(self):
-		if self.launch:
-			rospy.loginfo("Shutting down SLAM manager")
-			self.launch.shutdown()
-if __name__ == "__main__":
-	try:
-		manager = SlamManager()
-		rospy.spin()
-	except rospy.ROSInterruptException:
-		print("Some error happened")
-		pass
-	finally:
-		manager.shutdown()
+        # Paths
+        self.mapper_params_file = rospy.get_param('~mapper_params_file', '/path/to/mapper_params_online_sync.yaml')
+
+        # Process handlers
+        self.processes = []
+
+        # Publisher for status
+        self.status_pub = rospy.Publisher('/slam_status', String, queue_size=10)
+
+    def start_process(self, command):
+        """Start a subprocess and track it."""
+        rospy.loginfo(f"Starting process: {' '.join(command)}")
+        process = subprocess.Popen(command)
+        self.processes.append(process)
+
+    def start_teleop(self):
+        if self.enable_teleop:
+            self.start_process([
+                "rosrun", "turtlebot3_teleop", "turtlebot3_teleop_key"
+            ])
+
+    def start_gazebo(self):
+        if self.enable_gazebo:
+            self.start_process([
+                "roslaunch", "turtlebot3_gazebo", "turtlebot3_house.launch"
+            ])
+
+    def start_slam_toolbox(self):
+        if self.enable_slam_toolbox:
+            self.start_process([
+                "rosrun", "slam_toolbox", "sync_slam_toolbox_node",
+                "_params_file:=%s" % self.mapper_params_file
+            ])
+
+    def run(self):
+        rospy.loginfo("Starting SLAM Manager...")
+
+        # Start requested components
+        self.start_teleop()
+        rospy.sleep(1)  # Allow slight delay for initialization
+        self.start_gazebo()
+        rospy.sleep(5)  # Allow Gazebo some time to start
+        self.start_slam_toolbox()
+
+        # Periodically publish status
+        rate = rospy.Rate(1)
+        while not rospy.is_shutdown():
+            self.status_pub.publish(String(data="SLAM system running"))
+            rate.sleep()
+
+    def shutdown(self):
+        rospy.loginfo("Shutting down SLAM Manager...")
+        for process in self.processes:
+            process.terminate()
+            process.wait()
+
+if __name__ == '__main__':
+    manager = SLAMManager()
+    try:
+        manager.run()
+    except rospy.ROSInterruptException:
+        pass
+    finally:
+        manager.shutdown()
